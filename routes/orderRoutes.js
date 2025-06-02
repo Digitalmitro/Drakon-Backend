c8e8f99cf070aac963c6955d9753077d72914b4f
soumendigitalmitro <soumen.digitalmitro@gmail.com>
2025-06-02 19:14:49 +0530

    new fixes for orders

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const orderController = require("../controllers/orderController");
 const userAuth = require("../middlewares/userAuth");
 
-router.post("/order/create",userAuth, orderController.createOrder);
+router.post("/order/create", userAuth, orderController.createOrder);
 router.get("/user/:userId", orderController.getUserOrders);
 router.get("/:orderId", orderController.getOrderById);
 router.put("/update", orderController.updateOrderStatus);
-router.get("/order/allorder", orderController.getOrder);
-router.get("/create-order",orderController.createShippingOrder)
+router.get("/order/allorder", orderController.getAllOrders);
+router.get("/create-order", orderController.createShippingOrder)
 
 module.exports = router;
